2c5083bb0345110ff752c89ee885867e48db9328
yeasiniqra <yeasin@iqrasys.com>
2023-07-11 20:58:29 +0600

    salse page implement

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,11 +1,14 @@
-$(document).ready(function(){
-	$().click(function(){
-			$().slideToggle(700);
-			return false;
-		});	
+// Here are javascript code active menu by click
+const links = document.querySelectorAll(".nav-link");
 
-
-});
+links.forEach((link)=> {
+    link.addEventListener("click", function(){
+        links.forEach((e)=> {
+        	e.classList.remove('active')
+        })
+        this.classList.add('active')
+    })
+})
 
 
 
